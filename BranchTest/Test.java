b17e7eccf7703cb670f7106e5175ab4ee86eb3a6
PaulNijssenRNL <paul.nijssen.ricoh@outlook.com>
2021-03-11 10:03:42 +0100

    Starting point recovered.

--- BranchTest/Test.java
+++ BranchTest/Test.java
@@ -1,6 +1,9 @@
 class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Main finished");
-		printInfo();
+	}
+	
+	private static void printInfo() {
+		// Hier komt iets.
 	}
 }
