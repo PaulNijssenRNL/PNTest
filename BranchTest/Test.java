89757317944d791b2190c0e518387088884930b0
PaulNijssenRNL <paul.nijssen.ricoh@outlook.com>
2021-03-11 10:23:21 +0100

    Restored

--- BranchTest/Test.java
+++ BranchTest/Test.java
@@ -1,14 +1,11 @@
 class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Main finished");
-		printInfo();
 	}
 	
 	private static void printInfo() {
 		// Hier komt iets.
 	}
 	
-	private static void test() {
-		// Hier komt iets.
-	}
+	// Dit is commentaar.
 }
