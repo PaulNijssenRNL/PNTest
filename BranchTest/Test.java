ea3ffb65f25593378ed316f36a85c1f21b0509d8
PaulNijssenRNL <paul.nijssen.ricoh@outlook.com>
2021-03-11 10:14:31 +0100

    Some changes

--- BranchTest/Test.java
+++ BranchTest/Test.java
@@ -1,15 +1,13 @@
 class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Main finished");
-		printInfo();
-		blah("Blabla");
 	}
 	
 	private static void printInfo() {
 		// Hier komt iets.
 	}
 	
-	private static void blah(String x) {
-		// Etc.
+	private static void test() {
+		// Hier komt iets.
 	}
 }
